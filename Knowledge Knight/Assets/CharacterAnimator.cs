915d07616caf8f658c5e1a4de9ad01137dc480b3
Deepayan Sanyal <dsanyal@live.com>
2019-04-23 13:23:52 -0800

    Added Character Movement
    Character moves to waypoints using the CharacterControl class. In addition, the CharacterAnimator class was created to play animations.

---
+++ Knowledge Knight/Assets/CharacterAnimator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+
+[RequireComponent(typeof(SkeletonAnimation))]
+public class CharacterAnimator : MonoBehaviour
+{
+
+    #region InspectorAnimations
+    [SpineAnimation]
+    public string walkAnimation;
+
+    [SpineAnimation]
+    public string idleAnimation;
+
+    [SpineAnimation]
+    public string runAnimation;
+
+    [SpineAnimation]
+    public string jumpAnimation;
+
+    [SpineAnimation]
+    public string hitAnimation;
+
+    [SpineAnimation]
+    public string deathAnimation;
+
+    [SpineAnimation]
+    public string pointAnimation;
+    #endregion
+
+    private SkeletonAnimation skeletonAnimation;
+
+    public Spine.AnimationState animationState;
+    public Spine.Skeleton skeleton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skeletonAnimation = GetComponent<SkeletonAnimation>();
+        animationState = skeletonAnimation.AnimationState;
+        skeleton = skeletonAnimation.skeleton;
+    }
+
+    public void PlayWalkAnimation()
+    {
+        animationState.SetAnimation(0, walkAnimation, true);
+    }
+
+    public void PlayIdleAnimation()
+    {
+        animationState.SetAnimation(0, idleAnimation, true);
+
+    }
+}
---
+++ Knowledge Knight/Assets/CharacterControl.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CharacterAnimator))]
+public class CharacterControl : MonoBehaviour
+{
+
+    private bool isMoving = false;
+    private bool facingRight = true;
+    private float targetX;
+
+    public float walkingSpeed = 3f;
+
+    Rigidbody2D rb;
+    CharacterAnimator animator;
+
+    private Waypoint[] waypoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypoints = FindObjectsOfType<Waypoint>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<CharacterAnimator>();
+        animator.PlayIdleAnimation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Jump");
+            WalkToWaypoint();
+        }
+        
+        if(isMoving && rb.position.x >= targetX)
+        {
+            rb.velocity = new Vector3(0f, rb.velocity.y);
+            isMoving = false;
+            animator.PlayIdleAnimation();
+        }
+
+    }
+
+    void WalkToWaypoint()
+    {
+
+        List<Waypoint> waypointsToRight = new List<Waypoint>();
+
+        foreach (Waypoint w in waypoints)
+        {
+            
+            if(w.transform.position.x > rb.transform.position.x)
+            {
+                waypointsToRight.Add(w);
+            }
+        }
+
+        if (waypointsToRight.Count > 0)
+        {
+
+            float x = waypointsToRight[0].transform.position.x;
+            
+            for(int i = 1; i < waypointsToRight.Count; i++)
+            {
+                
+                float wX = waypointsToRight[i].transform.position.x;
+
+                if(x - rb.transform.position.x >
+                    wX - rb.transform.position.x)
+                {
+                    x = wX;
+                }
+            }
+
+            facingRight = true;
+            isMoving = true;
+
+            targetX = x;
+            rb.velocity = new Vector2(walkingSpeed, rb.velocity.y);
+
+            animator.PlayWalkAnimation();
+
+        }
+    }
+}
